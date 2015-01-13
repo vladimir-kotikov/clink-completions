8755c549fa524792ace8bc1bd943314ee68a1b0d
Vladimir Kotikov <an-selm@ya.ru>
2015-01-13 23:25:24 +0300

    [common] Fix indentation: tabs -> spaces

--- chocolatey.lua
+++ chocolatey.lua
@@ -13,56 +13,56 @@ local function packages(token)
 end
 
 local clist_parser = parser(
-		"-all", "-allversions",
-		"-lo", "-localonly",
-		"-pre", "-prerelease",
-		"-source")
+        "-all", "-allversions",
+        "-lo", "-localonly",
+        "-pre", "-prerelease",
+        "-source")
 
 local cinst_parser = parser(
-		-- TODO: Path to packages config.
-		-- See https://github.com/chocolatey/chocolatey/wiki/CommandsInstall#packagesconfig---v09813
-		{"all", "packages.config"},
-		"-ia", "-installArgs", "-installArguments",
-		"-ignoreDependencies",
-		"-notSilent",
-		"-overrideArguments",
-		"-params", "-parameters", "-packageparameters",
-		"-pre", "-prerelease",
-		"-source" .. parser({"ruby", "webpi", "cygwin", "python"}),
-		"-version",
-		"-x86", "-forcex86")
+        -- TODO: Path to packages config.
+        -- See https://github.com/chocolatey/chocolatey/wiki/CommandsInstall#packagesconfig---v09813
+        {"all", "packages.config"},
+        "-ia", "-installArgs", "-installArguments",
+        "-ignoreDependencies",
+        "-notSilent",
+        "-overrideArguments",
+        "-params", "-parameters", "-packageparameters",
+        "-pre", "-prerelease",
+        "-source" .. parser({"ruby", "webpi", "cygwin", "python"}),
+        "-version",
+        "-x86", "-forcex86")
 
 local cuninst_parser = parser({packages}, "-version")
 
 local cup_parser = parser(
-		{"all"},
-		"-pre", "-prerelease",
-		"-source" .. parser({"ruby", "webpi", "cygwin", "python"}))
+        {"all"},
+        "-pre", "-prerelease",
+        "-source" .. parser({"ruby", "webpi", "cygwin", "python"}))
 
 local csources_parser=parser({
-		"add"..parser("-name", "-source"),
-		"disable",
-		"enable",
-		"list",
-		"remove"})
+        "add"..parser("-name", "-source"),
+        "disable",
+        "enable",
+        "list",
+        "remove"})
 
 local cver_parser=parser("-source", "-pre", "-prerelease", "-lo", "-localonly")
 
 local chocolatey_parser = parser({
-	"install"..cinst_parser,
-	"installmissing",
-	"update"..cup_parser,
-	"list"..clist_parser,
-	"search"..clist_parser,
-	"sources"..csources_parser,
-	"help",
-	"version"..cver_parser,
-	"gem"..parser("-version"),
-	"python"..parser("-version"),
-	"webpi",
-	"windowsfeatures",
-	"uninstall"
-	}, "/?")
+    "install"..cinst_parser,
+    "installmissing",
+    "update"..cup_parser,
+    "list"..clist_parser,
+    "search"..clist_parser,
+    "sources"..csources_parser,
+    "help",
+    "version"..cver_parser,
+    "gem"..parser("-version"),
+    "python"..parser("-version"),
+    "webpi",
+    "windowsfeatures",
+    "uninstall"
+    }, "/?")
 
 clink.arg.register_parser("choco", chocolatey_parser)
 clink.arg.register_parser("chocolatey", chocolatey_parser)
