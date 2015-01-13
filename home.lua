4fd9caab698f614b2c8f8d9801ad8fa05c1ca019
Vladimir Kotikov <an-selm@ya.ru>
2015-01-14 00:04:37 +0300

    [common] Fix indentation: tabs -> spaces

--- home.lua
+++ home.lua
@@ -1,8 +1,8 @@
 local function tilde_match (text, f, l)
-	if text == '~' then
-		clink.add_match(clink.get_env('userprofile')..'\\')
-		return true
-	end
+    if text == '~' then
+        clink.add_match(clink.get_env('userprofile')..'\\')
+        return true
+    end
 end
 
 clink.register_match_generator(tilde_match, 1)
--- python_utils.lua
+++ python_utils.lua
@@ -1,58 +1,58 @@
 local parser = clink.arg.new_parser
 
 local ea_parser = parser(
-	"--verbose",
-	"--quiet", "-q",
-	"--dry-run", "-n",
-	"--help", "-h",
-	"--no-user-cfg",
-	"--prefix",
-	"--zip-ok", "-z",
-	"--multi-version", "-m",
-	"--upgrade", "-U",
-	"--install-dir", "-d",
-	"--script-dir", "-s",
-	"--exclude-scripts", "-x",
-	"--always-copy", "-a",
-	"--index-url", "-i",
-	"--find-links", "-f",
-	"--build-directory", "-b",
-	"--optimize", "-O",
-	"--record",
-	"--always-unzip", "-Z",
-	"--site-dirs", "-S",
-	"--editable", "-e",
-	"--no-deps", "-N",
-	"--allow-hosts", "-H",
-	"--local-snapshots-ok", "-l",
-	"--version",
-	"--no-find-links",
-	"--user",
-	"--help"
-	)
+    "--verbose",
+    "--quiet", "-q",
+    "--dry-run", "-n",
+    "--help", "-h",
+    "--no-user-cfg",
+    "--prefix",
+    "--zip-ok", "-z",
+    "--multi-version", "-m",
+    "--upgrade", "-U",
+    "--install-dir", "-d",
+    "--script-dir", "-s",
+    "--exclude-scripts", "-x",
+    "--always-copy", "-a",
+    "--index-url", "-i",
+    "--find-links", "-f",
+    "--build-directory", "-b",
+    "--optimize", "-O",
+    "--record",
+    "--always-unzip", "-Z",
+    "--site-dirs", "-S",
+    "--editable", "-e",
+    "--no-deps", "-N",
+    "--allow-hosts", "-H",
+    "--local-snapshots-ok", "-l",
+    "--version",
+    "--no-find-links",
+    "--user",
+    "--help"
+    )
 
 local pip_flags = parser(
-	"-h", "--help",
-	"-v", "--verbose",
-	"-V", "--version",
-	"-q", "--quiet",
-	"--log", "--proxy", "--timeout", "--exists", "--cert"
+    "-h", "--help",
+    "-v", "--verbose",
+    "-V", "--version",
+    "-q", "--quiet",
+    "--log", "--proxy", "--timeout", "--exists", "--cert"
 )
 
 local pip_parser = parser(
-	{
-		"install" .. pip_flags,
-		"uninstall" .. pip_flags,
-		"freeze" .. pip_flags,
-		"list" .. pip_flags,
-		"show" .. pip_flags,
-		"search" .. pip_flags,
-		"wheel" .. pip_flags,
-		"zip" .. pip_flags,
-		"unzip" .. pip_flags,
-		"bundle" .. pip_flags,
-		"help" .. pip_flags
-	})
+    {
+        "install" .. pip_flags,
+        "uninstall" .. pip_flags,
+        "freeze" .. pip_flags,
+        "list" .. pip_flags,
+        "show" .. pip_flags,
+        "search" .. pip_flags,
+        "wheel" .. pip_flags,
+        "zip" .. pip_flags,
+        "unzip" .. pip_flags,
+        "bundle" .. pip_flags,
+        "help" .. pip_flags
+    })
 
 clink.arg.register_parser("easy_install", ea_parser)
 clink.arg.register_parser("pip", pip_parser)
