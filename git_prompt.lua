5a63dba3e10f5f746fbb5f3520d7c06a2039cee5
Vladimir Kotikov <an-selm@ya.ru>
2015-03-10 23:27:44 +0300

    [git] Adds initial prompt behavior compatibility code

--- git_prompt.lua
+++ git_prompt.lua
@@ -1,13 +1,32 @@
 function git_prompt_filter()
     local head = io.open('.git/HEAD')
+    local colors = {
+        clean = "\x1b[1;37;40m",
+        dirty = "\x1b[31;1m",
+    }
+    -- TODO: there is no status detection now 
+    -- so color will always be a colors.clean
+    local color = colors.clean
+
     if head ~= nil then
-        h = head:read()
+        local h = head:read()
         local branch = string.match(h, "/([%w-]+)$")
-        if (branch) then
-            clink.prompt.value = color_text("["..branch.."]", "black", "white").." "..clink.prompt.value
+
+        if branch then
+            if clink.prompt.value:match("{git}") then
+                clink.prompt.value = string.gsub(clink.prompt.value, "{git}", color.."("..branch..")")
+            else
+                clink.prompt.value = color.."("..branch..") "..clink.prompt.value
+            end
+        else
+            clink.prompt.value = string.gsub(clink.prompt.value, "{git}", "")
         end
+
         head:close()
+    else 
+        clink.prompt.value = string.gsub(clink.prompt.value, "{git}", "")
     end
+
     return false
 end
 
