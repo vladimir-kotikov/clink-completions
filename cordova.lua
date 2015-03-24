63ecfa110ea9ce8950e83b231d761538ea050139
Vladimir Kotikov <v-vlkoti@microsoft.com>
2015-03-24 09:50:53 +0300

    [cordova] Syncs completions with actual CLI parameters for latest version

--- cordova.lua
+++ cordova.lua
@@ -33,49 +33,54 @@ local platform_add_parser = parser({
     "blackberry10",
     "firefoxos",
     dir_match_generator
-}):loop(1)
+}, "--usegit", "--save", "--link"):loop(1)
 
 local plugin_add_parser = parser({dir_match_generator,
-    "org.apache.cordova.battery-status",
-    "org.apache.cordova.camera",
-    "org.apache.cordova.contacts",
-    "org.apache.cordova.device",
-    "org.apache.cordova.device-motion",
-    "org.apache.cordova.device-orientation",
-    "org.apache.cordova.dialogs",
-    "org.apache.cordova.file",
-    "org.apache.cordova.file-transfer",
-    "org.apache.cordova.geolocation",
-    "org.apache.cordova.globalization",
-    "org.apache.cordova.inappbrowser",
-    "org.apache.cordova.media",
-    "org.apache.cordova.media-capture",
-    "org.apache.cordova.network-information",
-    "org.apache.cordova.splashscreen",
-    "org.apache.cordova.vibration"
-}):loop(1)
+        "org.apache.cordova.battery-status",
+        "org.apache.cordova.camera",
+        "org.apache.cordova.contacts",
+        "org.apache.cordova.device",
+        "org.apache.cordova.device-motion",
+        "org.apache.cordova.device-orientation",
+        "org.apache.cordova.dialogs",
+        "org.apache.cordova.file",
+        "org.apache.cordova.file-transfer",
+        "org.apache.cordova.geolocation",
+        "org.apache.cordova.globalization",
+        "org.apache.cordova.inappbrowser",
+        "org.apache.cordova.media",
+        "org.apache.cordova.media-capture",
+        "org.apache.cordova.network-information",
+        "org.apache.cordova.splashscreen",
+        "org.apache.cordova.vibration"
+    },
+    "--searchpath" ..parser({dir_match_generator}),
+    "--noregistry",
+    "--link",
+    "--save",
+    "--shrinkwrap"
+):loop(1)
 
-local platform_rm_parser = parser({platforms}):loop(1)
-local plugin_rm_parser = parser({plugins}, "-f", "--force"):loop(1)
+local platform_rm_parser = parser({platforms}, "--save"):loop(1)
+local plugin_rm_parser = parser({plugins}, "-f", "--force", "--save"):loop(1)
 
 local cordova_parser = parser(
     {
-    -- common commands
+        -- common commands
         "create" .. parser(
-            "--copy-from",
-            "--src" .. parser({dir_match_generator}),
-            "--link-to=" .. parser({dir_match_generator})
+            "--copy-from", "--src",
+            "--link-to"
         ),
         "help",
+        -- project-level commands
         "info",
-    -- project-level commands
         "platform" .. parser({
             "add" .. platform_add_parser,
             "remove" .. platform_rm_parser,
             "rm" .. platform_rm_parser,
             "list", "ls",
             "up" .. parser({platforms}):loop(1),
-            "update" .. parser({platforms}):loop(1),
+            "update" .. parser({platforms}, "--usegit", "--save"):loop(1),
             "check"
             }),
         "plugin" .. parser({
@@ -84,8 +89,8 @@ local cordova_parser = parser(
             "rm" .. plugin_rm_parser,
             "list", "ls",
             "search"
-        }),
-        "prepare" .. parser({platforms}):loop(1),
+        }, "--browserify"),
+        "prepare" .. parser({platforms}, "--browserify"):loop(1),
         "compile" .. parser({platforms},
             "--debug", "--release",
             "--device", "--emulator", "--target="):loop(1),
@@ -98,7 +103,9 @@ local cordova_parser = parser(
             "--device", "--emulator", "--target="),
         "emulate" .. parser({platforms}),
         "serve",
-    }, "-h", "-d")
+    }, "-h",
+    "-v", "--version",
+    "-d", "--verbose")
 
 clink.arg.register_parser("cordova", cordova_parser)
 clink.arg.register_parser("cordova-dev", cordova_parser)
