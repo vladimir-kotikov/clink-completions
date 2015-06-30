b4fffd9b81fc454ddbd984edc8c66f57013c811c
Vladimir Kotikov <v-vlkoti@microsoft.com>
2015-06-30 09:40:29 +0300

    [common] Avoid blocking package.json when error happens in prompt filter.

--- npm_prompt.lua
+++ npm_prompt.lua
@@ -3,10 +3,11 @@ function npm_prompt_filter()
     local package = io.open('package.json')
     if package ~= nil then
         local package_info = package:read('*a')
+        package:close()
         local package_name = string.match(package_info, '"name"%s*:%s*"(%g-)"')
         local package_version = string.match(package_info, '"version"%s*:%s*"(.-)"')
         clink.prompt.value = color_text("["..package_name.."@"..package_version.."]", "black", "green").." "..clink.prompt.value
-        package:close()
+        clink.prompt.value:gsub('{git}', '{git} '..package_string)
     end
     return false
 end
