edbcdcd63e89daaf7e718966cc20eede1a0f7879
Vladimir Kotikov <an-selm@ya.ru>
2015-07-14 21:06:55 +0300

    [common] Removes stale scripts

--- home.lua
+++
@@ -1,8 +0,0 @@
-local function tilde_match (text, f, l)
-    if text == '~' then
-        clink.add_match(clink.get_env('userprofile')..'\\')
-        return true
-    end
-end
-
-clink.register_match_generator(tilde_match, 1)
