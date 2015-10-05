vladimir-kotikov/clink-completions

commit 751ed21a398a84085e57cc11087d30b81f04f953

[git] Fixes checkout completion to list branches correctly.
This fixes #30

git.lua
@@ -78,55 +78,32 @@ local function local_or_remote_branches(token)
     local git_dir = get_git_dir()
     if not git_dir then return {} end
 
-    -- find all remotes and filter out fake . and .. items
-    return w(clink.find_dirs(git_dir.."/refs/remotes/*"))
-    :filter(function (remote) return path.is_real_dir(remote) end)
-    -- do a reduce against list of remotes, passing list of local branches as accumulator
-    :reduce(branches(token), function(result, remote)
-        -- list all fetched branches for this remote filtering out fake . and .. entries
-        return w(clink.find_files(git_dir .. "/refs/remotes/" .. remote .. "/*"))
-        :filter(function (branch) return path.is_real_dir(branch) end)
-        -- construct refspec from remote and branch name and append it to accumulator
-        :map(function (branch) return remote..'/'..branch end)
-        :concat(result)
-    end)
-    :filter(function(result)
-        return clink.is_match(token, result)
+    return w(path.list_files(git_dir..'/refs/remotes', '/*', --[[recursive=]]true, --[[reverse_separator=]]true))
+    :filter(function(path)
+        return clink.is_match(token, path)
     end)
 end
 
 local function checkout_spec_generator(token)
+    local branches = local_or_remote_branches(token)
+    local files = matchers.files(token)
+    
+    if #branches == 0 then return files end
 
-    --TODO: rework this:
-    --  there is still dot/double dot in the completions list
-    --  there it no asterisks in front of some branches
-    --  there is no big difference between branches and files/directories
-
-    local res = {}
-    local res_filter = {}
-
-    for _,branch in ipairs(local_or_remote_branches(token)) do
-        table.insert(res, branch)
-        table.insert(res_filter, '*' .. branch)
-    end
-
-    for _,file in ipairs(matchers.files(token)) do
-        table.insert(res, file)
-        -- TODO: lines, inserted here contains all path, not only last segmentP
-
-        local prefix = path.basename(file)
-        if clink.is_dir(file) then
-            prefix = prefix..'\\'
-        end
-
-        table.insert(res_filter, prefix)
-    end
-
+    -- if there is any refspec that matches token then:
+    --   * disable readline's filename completion, otherwise we'll get a list of these specs
+    --     threaten as list of files (without 'path' part), ie. 'some_branch' instead of 'my_remote/some_branch'
+    --   * create display filter for completion table to append path separator to each directory entry
+    --     since it is not added automatically by readline (see previous point)
+    clink.matches_are_files(0)
     clink.match_display_filter = function (matches)
-        return res_filter
+        return files:map(function(file)
+            return clink.is_dir(file) and file..'\\' or file
+        end)
+        :concat(branches)
     end
-
-    return res
+    
+    return files:concat(branches)
 end
 
 local stashes = function(token)

commit c8e1ac5b2a60b1e04acf402f900d8de5a0bdc263

[git] Adds local branches to `git checkout` (was broken by 751ed21)

git.lua
@@ -35,7 +35,8 @@ local branches = function (token)
     local git_dir = get_git_dir()
     if not git_dir then return {} end
 
-    return w(path.list_files(git_dir..'/refs/heads', '/*', --[[recursive=]]true, --[[reverse_separator=]]true))
+    return w(path.list_files(git_dir..'/refs/heads', '/*',
+        --[[recursive=]]true, --[[reverse_separator=]]true))
     :filter(function(path)
         return clink.is_match(token, path)
     end)
@@ -78,7 +79,9 @@ local function local_or_remote_branches(token)
     local git_dir = get_git_dir()
     if not git_dir then return {} end
 
-    return w(path.list_files(git_dir..'/refs/remotes', '/*', --[[recursive=]]true, --[[reverse_separator=]]true))
+    return w(path.list_files(git_dir..'/refs/remotes', '/*',
+        --[[recursive=]]true, --[[reverse_separator=]]true))
+    :concat(branches(token))
     :filter(function(path)
         return clink.is_match(token, path)
     end)
@@ -87,7 +90,7 @@ end
 local function checkout_spec_generator(token)
     local branches = local_or_remote_branches(token)
     local files = matchers.files(token)
-    
+
     if #branches == 0 then return files end
 
     -- if there is any refspec that matches token then:
@@ -102,7 +105,7 @@ local function checkout_spec_generator(token)
         end)
         :concat(branches)
     end
-    
+
     return files:concat(branches)
 end
 
@@ -120,7 +123,7 @@ local function push_branch_spec(token)
     -- * if there is no branch separator complete word with local branches
     if not s then
         local b = branches(branch_spec)
-        
+
         -- setup display filter to prevent display '+' symbol in completion list
         clink.match_display_filter = function ()
             return b
@@ -533,7 +536,7 @@ local git_parser = parser(
         "prune",
         "prune-packed",
         "pull" .. parser(
-            {remotes}, {branches}, 
+            {remotes}, {branches},
             "-q", "--quiet",
             "-v", "--verbose",
             "--recurse-submodules", --[no-]recurse-submodules[=yes|on-demand|no]
@@ -710,7 +713,7 @@ local git_parser = parser(
 				"--include-paths", "--no-minimize-url", "--preserve-empty-dirs",
 				"--placeholder-filename"),
 				"rebase"..parser({local_or_remote_branches}, {branches}),
-			"dcommit"..parser("--no-rebase", "--commit-url", "--mergeinfo", "--interactive"), 
+			"dcommit"..parser("--no-rebase", "--commit-url", "--mergeinfo", "--interactive"),
 			"branch"..parser("-m","--message","-t", "--tags", "-d", "--destination", "--username", "--commit-url", "--parents"),
 			"log"..parser("-r", "--revision", "-v", "--verbose", "--limit", "--incremental", "--show-commit", "--oneline"),
 			"find-rev"..parser("--before", "--after"),