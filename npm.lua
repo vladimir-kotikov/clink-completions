c20e421980485b8184b035664c03155e4a1ad7ca
Vladimir Kotikov <an-selm@ya.ru>
2015-10-15 00:59:59 +0300

    [common] Merge npm prompt into regular 'npm' module

--- npm.lua
+++ npm.lua
@@ -1,5 +1,6 @@
 -- preamble: common routines
 
+local color = require('color')
 local matchers = require('matchers')
 
 function trim(s)
@@ -176,3 +177,18 @@ local npm_parser = parser({
 )
 
 clink.arg.register_parser("npm", npm_parser)
+
+function npm_prompt_filter()
+    local package = io.open('package.json')
+    if package ~= nil then
+        local package_info = package:read('*a')
+        package:close()
+        local package_name = string.match(package_info, '"name"%s*:%s*"(.-)"')
+        local package_version = string.match(package_info, '"version"%s*:%s*"(.-)"')
+        local package_string = color.color_text("("..package_name.."@"..package_version..")", color.YELLOW)
+        clink.prompt.value = clink.prompt.value:gsub('{git}', '{git} '..package_string)
+    end
+    return false
+end
+
+clink.prompt.register_filter(npm_prompt_filter, 40)
