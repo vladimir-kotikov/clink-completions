1cea322bd6f579e8b8798381fcdd928a3869ce74
Vladimir Kotikov <kotikov.vladimir@gmail.com>
2015-10-24 15:55:03 +0300

    [npm] Fix npm prompt failure when parsing malformed package.json

--- npm.lua
+++ npm.lua
@@ -186,7 +186,11 @@ function npm_prompt_filter()
         local package_info = package:read('*a')
         package:close()
         local package_name = string.match(package_info, '"name"%s*:%s*"(.-)"')
+            or "<invalid_name>"
+
         local package_version = string.match(package_info, '"version"%s*:%s*"(.-)"')
+            or "<invalid_version>"
+
         local package_string = color.color_text("("..package_name.."@"..package_version..")", color.YELLOW)
         clink.prompt.value = clink.prompt.value:gsub('{git}', '{git} '..package_string)
     end
