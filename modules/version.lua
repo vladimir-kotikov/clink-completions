0f6319313c6791b65652608836794f41690afdc7
Chris Antos <sparrowhawk996@gmail.com>
2021-01-04 19:11:18 -0800

    Busted runs the modules scripts outside of Clink.
    The version module is meant to encapsulate Clink versioning, but it must
    do so without relying on Clink being present.

--- modules/version.lua
+++ modules/version.lua
@@ -1,5 +1,9 @@
 local exports = {}
 
+-- Busted runs these modules scripts *outside* of Clink.
+-- So these Clink scripts have to work without any Clink APIs being available.
+clink = clink or {}
+
 local clink_version_encoded = clink.version_encoded or 0
 
 exports.supports_display_filter_description = function ()
