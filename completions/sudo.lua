9c0e3db436ee6518fd91561b4ed6d6d0d9db0cfb
Chris Antos <sparrowhawk996@gmail.com>
2023-02-12 09:21:44 -0800

    Clean luacheck warnings.

--- completions/sudo.lua
+++ completions/sudo.lua
@@ -6,19 +6,22 @@ end
 
 local sudo = clink.argmatcher("sudo"):chaincommand()
 
+-- luacheck: globals os
+
 -- Detect when sudo from https://github.com/chrisant996/sudo-windows is being
 -- used, and parse its help text to build a full argmatcher.
 if os.getfileversion then
 
-    local function delayinit(argmatcher, command_word)
+    local function closure(argmatcher)
+        argmatcher:chaincommand()
+    end
+
+    local function delayinit(argmatcher, command_word) -- luacheck: no unused
         if string.lower(path.getname(command_word)) == "sudo.exe" then
             local info = os.getfileversion(command_word)
             if info and info.companyname == "Christopher Antos" then
                 local help_parser = require("help_parser")
                 if help_parser then
-                    local function closure(argmatcher)
-                        argmatcher:chaincommand()
-                    end
                     help_parser.make(command_word, "-?", "gnu", nil, closure)
                 end
             end
