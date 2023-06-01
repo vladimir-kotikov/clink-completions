7a6e0559e25d7fcd877f64c49539c08d281f6aa6
Chris Antos <sparrowhawk996@gmail.com>
2023-06-01 01:27:08 -0700

    Fix #175; update ssh and ping completions.

--- completions/ping.lua
+++ completions/ping.lua
@@ -1,5 +1,6 @@
 require('arghelper')
 local w = require('tables').wrap
+local clink_version = require('clink_version')
 
 -- Hosts from the .ssh/config file use `color.alias`.
 -- Hosts from the .ssh/known_hosts use `color.cmd`.
@@ -24,41 +25,64 @@ local function read_lines (filename)
     return lines
 end
 
--- read all Host entries in the user's ssh config file
-local function list_ssh_hosts()
-    return read_lines(os.getenv("userprofile") .. "/.ssh/config")
-        :map(function (line)
-            local host = line:match('^Host%s+(.*)$')
-            if host then
-                for pattern in host:gmatch('([^%s]+)') do
-                    if not pattern:match('[%*|%?|/|!]') then
-                        return pattern
-                    end
-                end
-            end
-            return nil
-        end)
-        :filter()
-        :map(function(line)
-            return { match=line, type="alias" }
-        end)
+local function extract_address(pattern, match_type, portflag)
+    if not pattern then
+        return nil
+    end
+
+    local addr, port = pattern:match('%[([^%]]+)%]:(%d+)')
+    if not addr then
+        addr = pattern:match('%[([^%]]+)%]')
+    end
+    if not addr then
+        addr = pattern
+    end
+
+    local match
+    if portflag and port then
+        match = addr .. portflag .. port
+    else
+        match = addr
+    end
+    if clink_version.supports_display_filter_description then
+        return { match=match, type=match_type }
+    else
+        return match
+    end
 end
 
-local function list_known_hosts()
-    return read_lines(os.getenv("userprofile") .. "/.ssh/known_hosts")
+-- read all Host entries in the user's ssh config file
+local function list_ssh_hosts(portflag)
+    local matches = w({})
+    local lines = read_lines(clink.get_env("userprofile") .. "/.ssh/config")
+    for _, line in ipairs(lines) do
+        line = line:gsub('(#.*)$', '')
+        local host = line:match('^Host%s+(.*)$')
+        if host then
+            for pattern in host:gmatch('([^%s]+)') do
+                if not pattern:match('[%*%?/!]') then
+                    table.insert(matches, extract_address(pattern, 'alias', portflag))
+                end
+            end
+        end
+    end
+    return matches:filter()
+end
+
+local function list_known_hosts(portflag)
+    return read_lines(clink.get_env("userprofile") .. "/.ssh/known_hosts")
         :map(function (line)
-            return line:match('^([^%s,]*).*')
+            line = line:gsub('(#.*)$', '')
+            return extract_address(line:match('^([^%s,]*).*'), 'cmd', portflag)
         end)
         :filter()
-        :map(function (line)
-            return { match=line, type="cmd" }
-        end)
 end
 
 local function list_hosts_file()
     local t = w({})
     local lines = read_lines(os.getenv("systemroot") .. "/system32/drivers/etc/hosts")
     for _, line in ipairs(lines) do
+        line = line:gsub('(#.*)$', '')
         local ip, hosts = line:match('^%s*([0-9.:]+)%s(.*)$')
         if ip then
             table.insert(t, ip)
@@ -70,7 +94,7 @@ local function list_hosts_file()
     return t:filter()
 end
 
-local hosts = function (token)  -- luacheck: no unused args
+local function hosts(token)  -- luacheck: no unused args
     return list_ssh_hosts()
         :concat(list_known_hosts())
         :concat(list_hosts_file())
