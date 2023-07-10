bcfcfac36e7bb7b04b839fdd513aaab962c60ae9
Chris Antos <sparrowhawk996@gmail.com>
2023-07-10 09:40:49 -0700

    Perf improvement in edge case.
    The git_prompt.lua prompt filter tries to add " -> remote" to the branch
    name in the prompt, and it loads the git config file to do so.
    
    In some repos, the git config file may be large enough that it could
    cause a perceivable delay.  Using a prompt coroutine enables caching the
    result so that the config file is only loaded once per input prompt,
    even if other async prompt filters trigger multiple refreshes.
    
    Tested with Clink 1.5.1:
    - Outside a git repo.
    - Inside a git repo with a remote.
    - Inside a git repo without a remote.
    
    Tested with Clink 1.1.12 (does not support prompt coroutines):
    - Outside a git repo.
    - Inside a git repo with a remote.
    - Inside a git repo without a remote.

--- git_prompt.lua
+++ git_prompt.lua
@@ -3,6 +3,24 @@ local gitutil = require('gitutil')
 
 -- TODO: cache config based on some modification indicator (system mtime, hash)
 
+---
+-- Forward/backward compatibility for Clink asynchronous prompt filtering.
+-- With Clink v1.2.10 and higher this lets git status run in the background and
+-- refresh the prompt when it finishes, to eliminate waits in large git repos.
+---
+local io_popenyield
+local clink_promptcoroutine
+local cached_info = {}
+if clink.promptcoroutine and io.popenyield then
+    io_popenyield = io.popenyield
+    clink_promptcoroutine = clink.promptcoroutine
+else
+    io_popenyield = io.popen
+    clink_promptcoroutine = function (func)
+        return func(false)
+    end
+end
+
 local function load_git_config(git_dir)
     if not git_dir then return nil end
     local file = io.open(git_dir.."/config", 'r')
@@ -50,6 +68,39 @@ local function get_git_config_value(git_config, section, param)
     return git_config[section] and git_config[section][param] or nil
 end
 
+---
+-- Use a prompt coroutine to do remote and ref resolution in the background.
+--
+-- Even though this doesn't spawn `git config` (which can be slow in some
+-- repos), it does load the config file and in some repos that might be large.
+-- So, use a prompt coroutine to enable caching the results so that it's only
+-- loaded once per prompt refresh.  I.e. when other async prompt filters
+-- finish and refresh the prompt, this uses the cached value instead of
+-- recomputing it each time.  This is probably just a very tiny optimization,
+-- but there might be some repos where it makes a noticeable difference.
+---
+local function get_git_remote(git_dir, branch)
+    local info = clink_promptcoroutine(function ()
+        -- for remote and ref resolution algorithm see https://git-scm.com/docs/git-push
+        local git_config = load_git_config(git_dir)
+        local remote_to_push = get_git_config_value(git_config, 'branch "'..branch..'"', 'remote') or ''
+        local remote_ref = get_git_config_value(git_config, 'remote "'..remote_to_push..'"', 'push') or
+            get_git_config_value(git_config, 'push', 'default')
+
+        local text = remote_to_push
+        if remote_ref then text = text..'/'..remote_ref end
+
+        if text == '' then return {} end
+        return { branch=branch, remote=text }
+    end)
+    if not info then
+        info = cached_info.info or {}
+    else
+        cached_info.info = info
+    end
+    return info
+end
+
 local function git_prompt_filter()
     -- Check for Cmder configured Git Status Opt In/Out - See: https://github.com/cmderdev/cmder/issues/2484
     if cmderGitStatusOptIn == false then return false end  -- luacheck: globals cmderGitStatusOptIn
@@ -70,17 +121,10 @@ local function git_prompt_filter()
     local branch = gitutil.get_git_branch(git_dir)
     if not branch then return false end
 
-    -- for remote and ref resolution algorithm see https://git-scm.com/docs/git-push
-    local git_config = load_git_config(git_dir)
-    local remote_to_push = get_git_config_value(git_config, 'branch "'..branch..'"', 'remote') or ''
-    local remote_ref = get_git_config_value(git_config, 'remote "'..remote_to_push..'"', 'push') or
-        get_git_config_value(git_config, 'push', 'default')
-
-    local text = remote_to_push
-    if (remote_ref) then text = text..'/'..remote_ref end
-
-    if (text ~= '') then
-      clink.prompt.value = clink.prompt.value:gsub(escape_find_arg('('..branch), '%1 -> '..escape_replace_arg(text))
+    -- Replace "(branch" with "(branch -> remote".
+    local info = get_git_remote(git_dir, branch)
+    if info.branch and info.remote then
+      clink.prompt.value = clink.prompt.value:gsub(escape_find_arg('('..info.branch), '%1 -> '..escape_replace_arg(info.remote))
     end
 
     return false
